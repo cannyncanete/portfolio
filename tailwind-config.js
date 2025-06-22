0abd2f67c75b38dae07443065e48f412b6bd2e0b
Canny <canny.n.canete@gmail.com>
2025-06-22 11:20:45 +0800

    Initial commit

---
+++ tailwind-config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          primary: '#1E40AF', // example color, feel free to change
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+  }
+  
